57ad0dae990883fcc50a83ca3f6dbe0533a2c864
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-06-23 12:32:33 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,44 @@
+class Disjoint{
+public:
+  vector<int>rank,parent;
+  Disjoint(int n){
+    rank.resize(n+1,0);
+    parent.resize(n+1);
+    for(int i=0;i<=n;i++){
+        parent[i]=i;
+    }
+  }
+  int ult_parent(int node){
+      if(node==parent[node])return node;
+      return parent[node]=ult_parent(parent[node]);
+  }
+  bool rank_by_union(int u,int v){
+      int pu=ult_parent(u);
+      int pv=ult_parent(v);
+      if(pu==pv)return true;
+      if(rank[pu]<rank[pv]){
+          parent[pu]=pv;
+      }
+      else if(rank[pv]<rank[pu]){
+          parent[pv]=pu;
+      }else{
+          parent[pv]=pu;
+          rank[pu]++;
+      }
+      return false;
+  }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        Disjoint ds(edges.size());
+        for(auto e:edges){
+            int u=e[0];
+            int v=e[1];
+            if(ds.rank_by_union(u,v)){
+                return e;
+            }
+        }
+        return {};
+    }
+};
