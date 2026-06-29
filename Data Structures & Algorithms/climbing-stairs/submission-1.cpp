1a1f1aa2838d9bf4961d53e0de6e4270af4f838e
Ramkumar <151425655+Ram9219@users.noreply.github.com>
2026-06-29 10:42:24 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int dp[10001];
+
+    int solve(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
+    }
+
+    int climbStairs(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
+    }
+};
